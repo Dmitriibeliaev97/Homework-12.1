26d36dd2ceb8a705f5d2a3cb5e0227bddc2cd28b
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-03-01 19:49:50 +0300

    Homework is done

---
+++ src/pro/sky/java/course1/lesson1/Homework1.java
@@ -0,0 +1,20 @@
+package pro.sky.java.course1.lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Author stephenKing = new Author("Stephen", "King");
+        Author joanneRowling = new Author("Joanne", "Rowling");
+        Book it = new Book("It", 1986, stephenKing);
+        Book harryPotter = new Book("Harry Potter", 1997, joanneRowling);
+
+        System.out.println("Автор - " + stephenKing.getName() + " " + stephenKing.getSurname());
+        System.out.println("Книга - " + it.getNameBook() + ", " + it.getYearOfPublication() + " год публикации, " + "Автор - " + stephenKing.getName() + " " + stephenKing.getSurname());
+        System.out.println("Автор - " + joanneRowling.getName() + " " + joanneRowling.getSurname());
+        System.out.println("Книга - " + harryPotter.getNameBook() + ", " + harryPotter.getYearOfPublication() + " год публикации, " + "Автор - " + joanneRowling.getName() + " " + joanneRowling.getSurname());
+
+        harryPotter.setYearOfPublication(1998);
+
+        System.out.println("Книга - " + harryPotter.getNameBook() + ", " + harryPotter.getYearOfPublication() + " год публикации, " + "Автор - " + joanneRowling.getName() + " " + joanneRowling.getSurname());
+
+    }
+}
