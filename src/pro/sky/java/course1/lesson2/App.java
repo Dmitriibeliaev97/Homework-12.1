9b5ed6e636c6cf64e9342750762fb88bac234402
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-03-02 21:03:21 +0300

    Скопировал код из прошлого ДЗ для начала выполнения нового ДЗ

---
+++ src/pro/sky/java/course1/lesson2/App.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course1.lesson2;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/pro/sky/java/course1/lesson2/Book.java
@@ -0,0 +1,29 @@
+package pro.sky.java.course1.lesson2;
+
+public class Book {
+    private String nameBook;
+    private int yearOfPublication;
+    private Author authorName;
+
+    public Book(String nameBook, int yearOfPublication, Author authorName) {
+        this.nameBook = nameBook;
+        this.yearOfPublication = yearOfPublication;
+        this.authorName = authorName;
+    }
+
+    public String getNameBook() {
+        return this.nameBook;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public Author getAuthorName() {
+        return this.authorName;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
